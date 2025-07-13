44db616df929e6f60570d5e1cf62bca9b726e47c
mrcodertashu <atrakedgamerz@gmail.com>
2025-07-13 15:14:03 +0530

    Update preloader.js

---
+++ jsFiles/preloader.js
@@ -0,0 +1,6 @@
+const preloader = document.getElementById('preloader');
+
+window.addEventListener('load', function () {
+    preloader.style.opacity = '0';
+    preloader.style.visibility = 'hidden';
+});
